35706079025bebe271c1d39993c7a97946f7d6a5
Abhishek Bansal <abhishek.oneplus.0105@gmail.com>
2022-04-21 01:58:01 +0530

    major changes, removed classes

--- Heston.py
+++ Heston.py
@@ -7,66 +7,34 @@ Created on Sat Apr 16 16:12:04 2022
 
 import numpy as np
 
-class Heston:
-    def __init__(self):
-        self.kappa = None
-        self.vbar = None
-        self.gamma = None
-        self.rho = None
-        self.v0 = None
-        self.S0 = None
-        self.rfr = None
-        self.maturity = None
+"""
+Class: Heston Model
+Implements Heston model characterstic function, montecarlo methods
+:parameters
+kappa = mean reversion speed
+vbar = long run variance
+gamma = vol of vol
+rho = correlation
+v0 = initial variance
+S0 = stock price
+rfr = risk free rate
+T = time to maturity
+"""
 
-        """
-        Class: Heston Model
-        Implements Heston model characterstic function, montecarlo methods
-        :parameters
-        kappa = mean reversion speed
-        vbar = long run variance
-        gamma = vol of vol
-        rho = correlation
-        v0 = initial variance
-        S0 = stock price
-        rfr = risk free rate
-        T = time to maturity
-        """
+def Heston_Char_func(u, kappa, vbar, gamma, rho,v0,rfr,maturity):
+    D1 = lambda u: np.sqrt(np.power(kappa - gamma*rho*1j*u,2)
+                           + (u**2 + 1j*u)*(gamma**2))
+    g_minus = lambda u: (kappa - 1j*rho*gamma*u - D1(u))
+    g_plus = lambda u: (kappa - 1j*rho*gamma*u + D1(u))
+    g = lambda u: g_minus(u)/g_plus(u)
+    e_d1 = lambda u: np.exp(-D1(u)*maturity)
 
-    def Heston_Char_func(self,u):
-        D1 = lambda u: np.sqrt(np.power(self.kappa - self.gamma*self.rho*1j*u,2)
-                               + (u**2 + 1j*u)*(self.gamma**2))
-        g_minus = lambda u: (self.kappa - 1j*self.rho*self.gamma*u - D1(u))
-        g_plus = lambda u: (self.kappa - 1j*self.rho*self.gamma*u + D1(u))
-        g = lambda u: g_minus(u)/g_plus(u)
-        e_d1 = lambda u: np.exp(-D1(u)*self.maturity)
+    val1 = lambda u: ((1j*u*rfr*maturity) + (v0/gamma**2)*
+                            ((1-e_d1(u))/(1-g(u)*e_d1(u)))*g_minus(u))
+    val2 = lambda u: ((kappa*vbar/gamma**2)*
+                            (maturity*g_minus(u) - 2*np.log((1-g(u)*e_d1(u))/(1-g(u)))))
+    cf = np.exp(val1(u))* np.exp(val2(u))
+    return cf
 
-        val1 = lambda u: ((1j*u*self.rfr*self.maturity) + (self.v0/self.gamma**2)*
-                                ((1-e_d1(u))/(1-g(u)*e_d1(u)))*g_minus(u))
-        val2 = lambda u: ((self.kappa*self.vbar/self.gamma**2)*
-                                (self.maturity*g_minus(u) - 2*np.log((1-g(u)*e_d1(u))/(1-g(u)))))
-        cf = np.exp(val1(u))* np.exp(val2(u))
-        return cf
-
-    def _adj_Heston_Char_func(self,u):
-        return self.Heston_Char_func(u - 1j)/self.Heston_Char_func(-1j)
-
-    def initializeHestonParameters(self, kappa, vbar, gamma, rho,v0,
-                                   S0 = None, rfr = None, mat = None):
-        self.kappa = kappa
-        self._kappa = kappa
-        self.vbar = vbar
-        self._vbar = vbar
-        self.gamma = gamma
-        self._gamma = gamma
-        self.rho = rho
-        self._rho = rho
-        self.v0 = v0
-        self._v0 = v0
-        self.S0 = S0
-        self._S0 = S0
-        self.rfr = rfr
-        self._rfr = rfr
-        self.maturity = mat
-        self._maturity = mat
-
-
+def _adj_Heston_Char_func(self,u, kappa, vbar, gamma, rho,v0,rfr,maturity):
+    return Heston_Char_func(u - 1j,kappa, vbar, gamma, rho,v0,rfr,maturity)/Heston_Char_func(-1j, kappa, vbar, gamma, rho,v0,rfr,maturity)
--- OptionPricing.py
+++ OptionPricing.py
@@ -4,141 +4,131 @@ Created on Sat Apr 16 20:12:04 2022
 
 @author: abhishek bansal
 """
+import os, sys
+CURRENT_DIR = os.path.dirname(os.path.abspath("/Users/abhishek/PycharmProjects/HestonModel/Heston.py"))
+sys.path.append(os.path.dirname(CURRENT_DIR))
+
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
-from scipy.optimize import minimize
-from Heston import Heston
+from scipy.optimize import minimize, brentq
+import HestonModel.Heston as Heston
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 outputPath = r"output/"
 
-class EuroOption(Heston):
-    def __init__(self, stockPrice, strike, vol, rate, maturity, dividend=0,optionType = "call"):
-        self.S0 = stockPrice
-        self.strike = strike
-        self.rfr = rate
-        self.vol = vol
-        self.maturity = maturity
-        self.q = dividend
+
+
+def price_BS_analytical(S0,strike,rfr,vol,maturity,optionType="call"):
+    if optionType == "call":
+        opType = 1
+    elif optionType == "put":
+        opType = -1
+
+    d1 = ((np.log(S0/strike)+(rfr + (vol**2)/2)*maturity)
+          /(vol*(np.sqrt(maturity))))
+    d2 = d1 - vol*np.sqrt(maturity)
+
+    return opType*(S0*norm.cdf(opType*d1) - strike*np.exp(-rfr*maturity)*norm.cdf(opType*d2))
+
+def BS_vega(self,S0,strike,rfr,vol,maturity):
+
+    d1 = ((np.log(S0/strike)+(rfr + (vol**2)/2)*maturity)
+          /(vol*(np.sqrt(maturity))))
+    return (S0*norm.pdf(d1)*np.sqrt(maturity))
+
+
+def getBS_ImpVol(targetPrice,S0,strike,rfr,maturity,optionType="call",MAX_ITERATIONS = 200,PRECISION = 1.0e-12):
+
+    def objFunc(vol,targetPrice,S0,strike,rfr,maturity,optionType):
+
+        price = price_BS_analytical(S0,strike,rfr,vol,maturity,optionType)
+        diff = price - targetPrice
+        return diff
+
+    def brentqc(errFunc,a,b,targetPrice,S0,strike,rfr,maturity,optionType):
+        try:
+            iv = brentq(errFunc, a, b, args=(targetPrice,S0,strike,rfr,maturity,optionType))
+        except:
+            iv = 0.000001
+        return iv
+    #df['iv'] = df.apply(lambda x: brentqc(objFunc,0.00001,50,args=(targetPrice,S0,strike,rfr,maturity,optionType="call"),axis=1)
+    #print(objFunc(sigma).shape)
+    # options = {'maxiter': MAX_ITERATIONS}
+    # vol = 0.001
+    # res = minimize(objFunc, vol,args=(targetPrice,S0,strike,rfr,maturity,optionType) ,method='Newton-CG', options=options,tol=PRECISION)
+
+
+    # for i in range(0, MAX_ITERATIONS):
+    #     price = self.price_BS_analytical(vol=sigma)[:,0]
+    #     vega = self.BS_vega(vol=sigma)
+    #     diff = price - targetPrice  # root
+    #     print((diff**2 < PRECISION).all())
+    #     if (diff**2 < PRECISION).all():
+    #         print("Price Diff is {}".format(diff))
+    #         return sigma
+    #     sigma = sigma - diff / vega  # f(x) / f'(x)
+    return brentqc(objFunc, 0.00001, 50, targetPrice,S0,strike,rfr,maturity,optionType)
+
+
+
+def price_Heston_CosMethod(S0,strike,kappa, vbar, gamma, rho,v0,rfr,maturity,a,b,N):
+
+    def upsilon_n(a,b,c,d,k):
+        npi_d = np.pi*k*(d - a)/(b-a)
+        npi_c = np.pi*k*(c - a)/(b-a)
+        val_one = (np.cos(npi_d)*np.exp(d) - np.cos(npi_c)*np.exp(c))
+        val_two = (k*np.pi/(b-a))*(np.sin(npi_d)*np.exp(d)-np.sin(npi_c)*np.exp(c))
+        return (val_one + val_two)/(1+(k*np.pi/(b-a))**2)
+
+    def psi_n(a,b,c,d,k):
+        if np.all(k==0):
+            return d-c
+        else:
+            return ((b-a)/(k*np.pi))*(np.sin(k*np.pi*(d - a)/(b-a))-np.sin(k*np.pi*(c-a)/(b-a)))
+
+    def H_k(a,b,k,optionType="call"):
         if optionType == "call":
-            self.optionType = 1
+            opType = 1
         elif optionType == "put":
-            self.optionType = -1
+            opType = -1
+        if opType == 1:
+            c = 0
+            d = b
+        elif opType == -1:
+            c = a
+            d = 0
+        return (opType*2/(b-a))*(upsilon_n(a,b,c,d,k) - psi_n(a,b,c,d,k))
 
-    def price_BS_analytical(self,S0=None,strike=None,rfr=None,vol=None,
-                            maturity=None,optionType=None):
-        S0 = self.S0 if S0 is None else S0
-        strike = self.strike if strike is None else strike
-        rfr = self.rfr if rfr is None else rfr
-        vol = self.vol if vol is None else vol
-        maturity = self.maturity if maturity is None else maturity
-        optionType = self.optionType if optionType is None else optionType
+    if strike is not np.array:
+        strike = np.array(strike).reshape([len(strike), 1])
 
-        d1 = ((np.log(S0/strike)+(rfr + (vol**2)/2)*maturity)
-              /(vol*(np.sqrt(maturity))))
-        d2 = d1 - vol*np.sqrt(maturity)
+    x0 = np.array([np.log(S0/i) for i in strike])
+    k = np.array([i for i in np.arange(1,N)])
+    price = np.empty((strike.shape[0],N))
+    price[:,0:1] = H_k(a,b,0)*Heston.Heston_Char_func(0,kappa, vbar, gamma, rho,v0,rfr,maturity)/2
+    price[:,1:] = H_k(a,b,k)*Heston.Heston_Char_func(k*np.pi/(b-a),kappa, vbar, gamma, rho,v0,rfr,maturity)*np.exp(1j*k*np.pi*(x0 - a)/(b-a))
 
-        return optionType*(S0*norm.cdf(optionType*d1)
-                                - strike*np.exp(-rfr*maturity)*norm.cdf(optionType*d2))
-
-    def BS_vega(self,S0=None,strike=None,rfr=None,vol=None,
-                            maturity=None,optionType=None):
-
-        S0 = self.S0 if S0 is None else S0
-        strike = self.strike if strike is None else strike
-        rfr = self.rfr if rfr is None else rfr
-        vol = self.vol if vol is None else vol
-        maturity = self.maturity if maturity is None else maturity
-        optionType = self.optionType if optionType is None else optionType
-
-        d1 = ((np.log(S0/strike)+(rfr + (vol**2)/2)*maturity)
-              /(vol*(np.sqrt(maturity))))
-        return (S0*norm.pdf(d1)*np.sqrt(maturity))[:,0]
-
-    def _getBS_ImpVol(self,targetPrice,MAX_ITERATIONS = 200,PRECISION = 1.0e-12):
-        #sigma = np.empty_like(self.price_BS_analytical().shape)
-        initGuess= 0.5
-        sigma = np.full_like(self.price_BS_analytical(), initGuess)# empty_like(self.price_BS_analytical().shape)
-        def objFunc(sigma):
-            sigma = sigma.reshape([len(sigma),1])
-            price = self.price_BS_analytical(vol=sigma)
-            diff = price - targetPrice.reshape(price.shape)
-            return sum(abs(diff))
-        #print(objFunc(sigma).shape)
-        options = {'maxiter': MAX_ITERATIONS}
-        res = minimize(objFunc, sigma, method='BFGS', options=options,tol=PRECISION)
-        # for i in range(0, MAX_ITERATIONS):
-        #     price = self.price_BS_analytical(vol=sigma)[:,0]
-        #     vega = self.BS_vega(vol=sigma)
-        #     diff = price - targetPrice  # root
-        #     print((diff**2 < PRECISION).all())
-        #     if (diff**2 < PRECISION).all():
-        #         print("Price Diff is {}".format(diff))
-        #         return sigma
-        #     sigma = sigma - diff / vega  # f(x) / f'(x)
-        return res.x
-
-    def getBS_IV(self):
-
-
-        return
-
-
-    def price_Heston_CosMethod(self,a,b,N):
-
-        def upsilon_n(a,b,c,d,k):
-            npi_d = np.pi*k*(d - a)/(b-a)
-            npi_c = np.pi*k*(c - a)/(b-a)
-            val_one = (np.cos(npi_d)*np.exp(d) - np.cos(npi_c)*np.exp(c))
-            val_two = (k*np.pi/(b-a))*(np.sin(npi_d)*np.exp(d)-np.sin(npi_c)*np.exp(c))
-            return (val_one + val_two)/(1+(k*np.pi/(b-a))**2)
-
-        def psi_n(a,b,c,d,k):
-            if np.all(k==0):
-                return d-c
-            else:
-                return ((b-a)/(k*np.pi))*(np.sin(k*np.pi*(d - a)/(b-a))-np.sin(k*np.pi*(c-a)/(b-a)))
-
-        def H_k(a,b,k):
-            if self.optionType == 1:
-                c = 0
-                d = b
-            elif self.optionType == -1:
-                c = a
-                d = 0
-            return (self.optionType*2/(b-a))*(upsilon_n(a,b,c,d,k) - psi_n(a,b,c,d,k))
-
-        if self.strike is not np.array:
-            self.strike = np.array(self.strike).reshape([len(self.strike),1])
-
-        x0 = np.array([np.log(self.S0/i) for i in self.strike])
-        k = np.array([i for i in np.arange(1,N)])
-        price = np.empty((self.strike.shape[0],N))
-        price[:,0:1] = H_k(a,b,0)*self.Heston_Char_func(0)/2
-
-        price[:,1:] = H_k(a,b,k)*self.Heston_Char_func(k*np.pi/(b-a))*np.exp(1j*k*np.pi*(x0 - a)/(b-a))
-
-        return np.diag(np.sum(price.real, axis = 1)*np.exp(-self.rfr*self.maturity)*self.strike)
+    return np.diag(np.sum(price.real, axis = 1)*np.exp(-rfr*maturity)*strike)
 
 #TODO: Correct the fourier method. Chnage the adjusted characterstic function in Heston
 
-    def price_FourierMethod(self,t_max,N):
-        delta_t = t_max/N
-        from_1_to_N = np.linspace(1,N,N)
-        t_n = (from_1_to_N - 1/2)*delta_t
-        price = dict()
-        for strike in self.strike:
-            k_log = np.log(self.S0/strike)
-            first_int = np.sum((((np.exp(-1j*t_n*k_log)*self._adj_Heston_Char_func(t_n)).imag/t_n)*delta_t),axis=0)
-            second_int = np.sum((((np.exp(-1j*t_n*k_log)*self.Heston_Char_func(t_n)).imag/t_n)*delta_t),axis=0)
-            if self.optionType == 1:
-                price[str(strike)] = (self.S0*(1/2 + first_int/np.pi)-np.exp(-self.rfr*self.maturity)*strike*(1/2 + second_int/np.pi))
-            else:
-                price[str(strike)] = (np.exp(-self.rfr*self.maturity)*strike*(1/2 - second_int/np.pi)-
-                                 self.S0*(1/2-first_int/np.pi))
-        return price
+def price_FourierMethod(t_max,N):
+    delta_t = t_max/N
+    from_1_to_N = np.linspace(1,N,N)
+    t_n = (from_1_to_N - 1/2)*delta_t
+    price = dict()
+    for strike in strike:
+        k_log = np.log(S0/strike)
+        first_int = np.sum((((np.exp(-1j*t_n*k_log)*_adj_Heston_Char_func(t_n)).imag/t_n)*delta_t),axis=0)
+        second_int = np.sum((((np.exp(-1j*t_n*k_log)*Heston_Char_func(t_n)).imag/t_n)*delta_t),axis=0)
+        if optionType == 1:
+            price[str(strike)] = (S0*(1/2 + first_int/np.pi)-np.exp(-rfr*maturity)*strike*(1/2 + second_int/np.pi))
+        else:
+            price[str(strike)] = (np.exp(-rfr*maturity)*strike*(1/2 - second_int/np.pi)-
+                             S0*(1/2-first_int/np.pi))
+    pass
 
 
 
@@ -172,7 +162,7 @@ if __name__ == "__main__":
     S0 = 100
     r = 0.03
     T = 0.5
-    N = 10000
+    N = 1000
     # K = np.array([95,100,105])
     K = np.array([90, 95, 100, 105, 110])
 
@@ -183,45 +173,59 @@ if __name__ == "__main__":
     # a = c1 - L*np.sqrt(c2 - np.sqrt(c4)) # we use +/- L*sqrt(T)
     # b = c1 + L*np.sqrt(c2 - np.sqrt(c4))
     start = time.time()
+    years = 25  # Farthest maturity
+    mat = [14 / 360, 30 / 360, 60 / 360, 0.5]
+    mat.extend([i for i in range(1, years + 1)])
+    # L = 10
+    # T = 0.5
+    # a = -L * np.sqrt(T)
+    # b = L * np.sqrt(T)
+    # prices = price_Heston_CosMethod(S0, K, kappa, vbar, gamma, rho, v0, rfr=r, maturity=T, a=a, b=b, N=N)
     for L in lst_L:
         loopstart = time.time()
-        years = 25  # Farthest maturity
-        mat = [7 / 360, 14 / 360, 30 / 360, 60 / 360, 0.5]
-        mat.extend([i for i in range(1, years + 1)])
         HestonPrices = np.empty([K.shape[0], len(mat)])
         BS_IVs = np.empty_like(HestonPrices)
         for i in range(0, len(mat)):
             T = mat[i]
-            option = EuroOption(S0, K, np.sqrt(v0), r, T)  # setting sigma(IV)^2 = v0
-            option.initializeHestonParameters(kappa, vbar, gamma, rho, v0, S0, r, T)
             a = -L * np.sqrt(T)
             b = L * np.sqrt(T)
-            prices = option.price_Heston_CosMethod(a, b, N)
+            prices = price_Heston_CosMethod(S0,K,kappa, vbar, gamma, rho,v0,rfr=r,maturity=T,a=a,b=b,N=N)
             HestonPrices[:, i] = prices
-            BS_IVs[:, i] = option._getBS_ImpVol(prices)
+
+        df_HestonPrices = pd.DataFrame(data = HestonPrices.T, index=mat, columns = K)
+        df_HestonPrices.reset_index(inplace=True)
+        #print(df_HestonPrices)
+        # print(df_HestonPrices.loc[90,:])
+        df_BS_IV = pd.DataFrame()
+        for strike in K:
+            df_BS_IV[strike] = df_HestonPrices.apply(lambda x: getBS_ImpVol(x[strike],100,
+                                                                                        strike,
+                                                                                        0.03,
+                                                                                        x['index'],
+                                                                                        "call"), axis=1)
 
         # Check if the BS analytical price matched using computed IV's
         BS_analyticalPrice = np.empty_like(HestonPrices)
         for i in range(0, len(mat)):
             T = mat[i]
-            sigma_new = BS_IVs[:, i]
-            option2 = EuroOption(S0, K, sigma_new, r, T)
-            BS_analyticalPrice[:, i] = option2.price_BS_analytical()
+            sigma_new = df_BS_IV.loc[i, :]
+            BS_analyticalPrice[:, i] = price_BS_analytical(S0, K, r, sigma_new, T)
+
 
         diff = HestonPrices - BS_analyticalPrice
         path_surface = outputPath + "BSImpVol_Surface_N{}k_L{}".format(int(N / 1000), L)
         path_diff = outputPath + "HestonVsBS_Diff_Surface_N{}k_L{}".format(int(N / 1000), L)
         path_priceSurface = outputPath + "HestonPrice_Surface_N{}k_L{}".format(int(N / 1000), L)
-        plot_surface(BS_IVs.T, K, mat, x_label="Strikes", y_label="Time to Maturity", z_label="BS Imp Vol",
+        plot_surface(df_BS_IV.to_numpy(), K, mat, x_label="Strikes", y_label="Time to Maturity", z_label="BS Imp Vol",
                      title="BS ImpVol Surface", save=True, filePath=path_surface+".png")
         plot_surface(diff.T, K, mat, x_label="Strikes", y_label="Time to Maturity", z_label="Diff",
                      title="Price Difference", save=True, filePath=path_diff+"_priceDiff.png")
         plot_surface(HestonPrices.T, K, mat, x_label="Strikes", y_label="Time to Maturity", z_label="BS Imp Vol",
                      title="Heston Surface", save=True, filePath=path_priceSurface+".png")
 
-        cols = ["1w","2w","1M","3M","6M"]
+        cols = ["2w","1M","3M","6M"]
         cols.extend(["{}Y".format(i) for i in range(1, years + 1)])
-        df_BS = pd.DataFrame(data=BS_IVs,
+        df_BS = pd.DataFrame(data=df_BS_IV.to_numpy().T,
                              index=K,
                              columns=cols)
         df_diff = pd.DataFrame(data=diff,
@@ -243,8 +247,8 @@ if __name__ == "__main__":
         df_diff.to_excel(writer, sheet_name='diff_HestonVsBSPrice', startrow=0, startcol=0)
         df_HestonPrices.to_excel(writer, sheet_name='HestonPrices', startrow=0, startcol=0)
         writer.save()
-    end = time.time()
-    print("Total runtime: {:.5f}s".format((end-start)))
+#     end = time.time()
+#     print("Total runtime: {:.5f}s".format((end-start)))
 # fig1, ax1 = plt.subplots()
 # for i in range(9,len(mat)):
 #     ax1.plot(K,data[:,i],label = "maturity {} years".format(mat[i]))
